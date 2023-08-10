95fe952012dc433eac6dc3c87c00bbae083a0835
xiaoyuanxun <53613219+xiaoyuanxun@users.noreply.github.com>
2023-08-10 22:26:14 +0800

    complete class2

--- print_character/src/mod_1/mod.rs
+++
@@ -1,8 +0,0 @@
-mod mod_2;
-pub use mod_2::print_A_to_z;
-
-pub fn print_a_to_Z() {
-    for ch in ('Z'..='a').rev() {
-        println!("{}", ch);
-    }
-}
--- print_character/src/mod_1/mod_2/mod.rs
+++
@@ -1,5 +0,0 @@
-pub fn print_A_to_z() {
-    for ch in 'A'..='z' {
-        println!("{}", ch);
-    }
-}
